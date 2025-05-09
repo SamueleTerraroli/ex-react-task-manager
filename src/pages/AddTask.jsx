50a816d065dac319f00cdcac57c0c5d4d6d8b4e6
SamueleTerraroli <terraroli99@gmail.com>
2025-05-09 11:38:26 +0200

    add: form addTask

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useState, useRef, useMemo } from 'react'
+
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\\",.<>?/`~";
 
 const AddTask = () => {
-    return (
-        <div>AddTask</div>
-    )
-}
+    const [taskTitle, setTaskTitle] = useState("");
+    const descriptionRef = useRef();
+    const statusRef = useRef();
 
-export default AddTask
+    const error = useMemo(() => {
+        if (taskTitle.trim() === "") {
+            return "Il nome del task non può essere vuoto.";
+        }
+        if ([...taskTitle].some(char => symbols.includes(char))) {
+            return "Il nome del task non può contenere simboli speciali.";
+        }
+        return "";
+    }, [taskTitle]);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        if (error)
+            return;
+
+        const newTask = {
+            title: taskTitle.trim(),
+            description: descriptionRef.current.value,
+            status: statusRef.current.value
+        }
+
+        console.log(newTask);
+    }
+
+    return (
+        <div>
+            <h1>Aggiungi Task</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Nome Task:
+                    <input
+                        type="text"
+                        value={taskTitle}
+                        onChange={e => setTaskTitle(e.target.value)}
+                    />
+                </label>
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                <label>Descrizione:
+                    <textarea ref={descriptionRef} />
+                </label>
+                <label>Stato:
+                    <select ref={statusRef} defaultValue="To do">
+                        <option value="To do">To do</option>
+                        <option value="Doing">Doing</option>
+                        <option value="Done">Done</option>
+                    </select>
+                </label>
+                <button type='submit' disabled={error}>
+                    Aggiungi task
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default AddTask;
